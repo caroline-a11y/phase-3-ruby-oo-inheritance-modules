71e01602255ff04816cf191bfb037044a70196c2
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-02-19 22:21:53 +0300

    completed assinment

---
+++ lib/dance_module.rb
@@ -0,0 +1,15 @@
+module Dance
+    def twirl
+        "I'm twirling!"
+    end
+    def jump
+        "look how high am jumping"
+    end
+    def pirouette
+        "I'm doing a pirouette"
+      end
+    def take_a_bow
+        Thank you, thank you. It was a pleasure to dance for you all."
+    end
+end
+
--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative '../fancy_dance.rb'
+
+class Dancer
+    extend FancyDance : :ClassMethods
+    include FancyDance : :InstanceMethods
+    atrr_accessor :name
+
+    def initializer (name)
+        @name = name
+    end
+
+end
